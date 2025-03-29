23c78b7a466952af29ada80c6c477e1cac126b6f
Kgm21 <karenmanchado19@gmail.com>
2025-03-29 12:43:03 -0300

    nuevos cambios

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
         <Row className="align-items-center text-center text-md-start">
         
           {/* Logo */}
-          <Col lg={4} md={3} sm={12} className="mb-3 mb-md-0 d-flex justify-content-center">
+          <Col lg={4} md={4} sm={12} className="mb-3 mb-md-0 d-flex justify-content-center">
             <img src="/img1.png" alt="DalePLay Logo" className="footer-logo" />
           </Col>
 
           {/* Navegación */}
-          <Col lg={4} md={3} sm={12} className="mb-3 mb-md-0 text-center">
+          <Col lg={4} md={4} sm={12} className="mb-3 mb-md-0 text-center">
             <Nav className="d-flex flex-column align-items-center">
               <Nav.Link href="#inicio" className="nav-link-custom">Inicio</Nav.Link>
               <Nav.Link href="#nosotros" className="nav-link-custom">Nosotros</Nav.Link>
@@ -24,7 +24,7 @@ const Footer = () => {
           </Col>
 
           {/* Redes sociales */}
-          <Col lg={4} md={3} sm={12} className="text-center">
+          <Col lg={4} md={4} sm={12} className="text-center">
             <h5>Síguenos</h5>
             <BsFacebook size={25} color="#1877F2" className="me-3" />
             <BsTwitter size={25} color="#1DA1F2" className="me-3" />
