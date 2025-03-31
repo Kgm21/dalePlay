291f7c70c903e9ba17cec602195490f7657ae907
Kgm21 <karenmanchado19@gmail.com>
2025-03-30 23:42:37 -0300

    inicio sesion

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,13 +11,13 @@ function Navbarcom() {
     <Navbar collapseOnSelect expand="lg" variant="dark" className="navbar-custom">
 
       <Container id='container' > 
-        <Navbar.Brand href="/inicio" className="navbar-brand-custom">
+        <Navbar.Brand href="/" className="navbar-brand-custom">
             <img src="./img1.png" alt="Logo" width="160" height="160" className="navbar-brand-logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto me-6">
-              <Nav.Link href="/inicio" className="nav-link-custom">Inicio</Nav.Link>
+              <Nav.Link href="/" className="nav-link-custom">Inicio</Nav.Link>
               <Nav.Link href="/iniciarSesion" className="nav-link-custom">Iniciar Sesión</Nav.Link>
               <Nav.Link href="/registro" className="nav-link-custom">Registro</Nav.Link>
               <Nav.Link href="/administracion" className="nav-link-custom">Administración</Nav.Link>
@@ -27,31 +27,7 @@ function Navbarcom() {
       </Container>
 
      
-      <style jsx>{`
-        @media (max-width: 991px) { 
-          .navbar-nav {
-            text-align: center !important; 
-            margin: 0 auto !important; 
-            display: flex;
-            justify-content: center;
-            flex-direction: column; 
-            padding: 10px 0; 
-          }
-          .nav-link {
-            margin: 5px 0 !important; 
-          }
-        }
-        @media (max-width: 768px) {
-          .navbar-brand {
-            top: 10px !important; 
-          }
-          .navbar-brand img {
-            width: 120px !important; 
-            height: 120px !important;
-            margin-right: 30px;
-          }
-        }
-      `}</style>
+     
     </Navbar>
   );
 }
--- src/paginas/IniciarSesion/iniciarSesion.jsx
+++ src/paginas/IniciarSesion/iniciarSesion.jsx
@@ -1,12 +1,63 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./iniciarSesion.css";
 
 const IniciarSesion = () => {
+  useEffect(() => {
+    document.body.classList.add("login-page");
+
+    return () => {
+      document.body.classList.remove("login-page");
+    };
+  }, []);
+
   return (
-    <div className='content'>
-      Iniciar sesion
+    <Container className="container">
+      <Row className="d-flex justify-content-start">
+        <Col md={4}>
+          <div className="form-container">
+            <h2 className="text-center">Iniciar Sesión</h2>
+            <Form>
+              <Form.Group className="mb-3" controlId="formUser">
+                <Form.Label>Usuario</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Ingrese su usuario"
+                  name="user"
+                  required 
+                />
+              </Form.Group>
 
-    </div>
-  )
-}
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Ingrese su contraseña"
+                  name="password"
+                  required 
+                />
+              </Form.Group>
 
-export default IniciarSesion
+              <Button variant="primary" type="submit" className="w-100">
+                Iniciar Sesión
+              </Button>
+            </Form>
+
+            
+            <div className="text-center mt-3">
+              <p className="form-label">
+                ¿No tienes cuenta?{" "}
+                <Link to="/registro" className="text-decoration-none">
+                  Regístrate aquí
+                </Link>
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default IniciarSesion;
