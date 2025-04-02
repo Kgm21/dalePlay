bd178768ea9aab7f8876f7c12b427a34498c9c83
Kgm21 <karenmanchado19@gmail.com>
2025-04-02 14:05:57 -0300

    r

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbarcom from './components/navbar/navbar'; 
 import Nosotros from './paginas/nosotros/nosotros';
-import Footer from './components/footer/Footer';
-import Inicio from './paginas/Inicio/Inicio';
-import Administracion from './paginas/Administracion/Administracion';
-import Registro from './paginas/Registro/Registro';
-import Contacto from './paginas/Contacto/Contacto';
+import Footer from './components/footer/footer';
+import Inicio from './paginas/inicio/inicio';
+import Administracion from './paginas/administracion/administracion';
+import Registro from './paginas/registro/registro';
+import Contacto from './paginas/contacto/contacto';
 import IniciarSesion from './paginas/IniciarSesion/iniciarSesion';
 import Error from './paginas/error_404/error';
+import DetallePelicula from './paginas/DetallePelicula/detallePelicula'
 
 function App() {
     
@@ -40,6 +41,7 @@ function App() {
                 <Route path='/contacto' element={<Contacto />} />
                 <Route path='/administracion' element={<Administracion />} />
                 <Route path='/error' element={<Error />} />
+                <Route path='/DetallePelicula' element={<DetallePelicula/>} />
             
             </Routes>
 
